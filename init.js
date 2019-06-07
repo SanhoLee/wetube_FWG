de904cde01fa09df6914157b01924957ed933537
SanhoLee <stlsh0516@gmail.com>
2019-06-07 20:19:24 +0900

    #2.2 MVC Patteran Part 3

--- init.js
+++ init.js
@@ -1,10 +1,10 @@
-// #2.1 MVC Patteran Part 2, up to setting object for URL
+// #2.2 MVC Patteran Part 3
 import app from "./app";
 // importing app object from app.js file
 
 const PORT = 4000;
 
-const handleListening = () => console.log(`We Listening on: http://localhost:${PORT}`);
+const handleListening = () => console.log(`✅We Listening on: http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
 // Listening app object something url change etc..
