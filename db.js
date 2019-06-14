772b9d9abca376b5c411e873544d2b0367defd41
SanhoLee <stlsh0516@gmail.com>
2019-06-14 23:08:48 +0900

    2.22 Home Controller part Two

--- db.js
+++ db.js
@@ -3,8 +3,8 @@ export const videos = [
     id: 1239123,
     title: "this is num 1 video",
     description: "hahaha",
-    views: 24,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    views: 14,
+    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
     creator: {
       id: 1232312,
       name: "sanho",
@@ -16,7 +16,7 @@ export const videos = [
     title: "this is num 2 video",
     description: "hadfsadhaha",
     views: 24,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
     creator: {
       id: 65565,
       name: "leon",
@@ -27,8 +27,8 @@ export const videos = [
     id: 1239123,
     title: "this is num 3 video",
     description: "lalala",
-    views: 24,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    views: 204,
+    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
     creator: {
       id: 212124,
       name: "john",
