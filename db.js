411e8016c6b06d261455f7ddf7116571ad36e443
SanhoLee <stlsh0516@gmail.com>
2019-06-18 20:18:08 +0900

    3.1 Connecting to MongoDB

--- db.js
+++ db.js
@@ -1,38 +1,16 @@
-export const videos = [
-  {
-    id: 1239123,
-    title: "this is num 1 video",
-    description: "hahaha",
-    views: 14,
-    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 1232312,
-      name: "sanho",
-      email: "nico@las.com"
-    }
-  },
-  {
-    id: 29483783,
-    title: "this is num 2 video",
-    description: "hadfsadhaha",
-    views: 24,
-    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 65565,
-      name: "leon",
-      email: "nico@las.com"
-    }
-  },
-  {
-    id: 1239123,
-    title: "this is num 3 video",
-    description: "lalala",
-    views: 204,
-    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 212124,
-      name: "john",
-      email: "nico@las.com"
-    }
-  }
-];
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/we-tube", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ Connected to DB");
+const handleError = error => console.log(`❌ Error on DB Connection:${error}`);
+
+
+
+db.once("open", handleOpen);
+db.on("error", handleError);
--- init.js
+++ init.js
@@ -1,9 +1,11 @@
+import "./db";
+
 import app from "./app";
 // importing app object from app.js file
 
 const PORT = 4000;
 
-const handleListening = () => console.log(`✅We Listening on: http://localhost:${PORT}`);
+const handleListening = () => console.log(`✅ We Listening on: http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
 // Listening app object something url change etc..
