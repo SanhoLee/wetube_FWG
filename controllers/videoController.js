ec9c87140cb8f34c5f28bc6f522f8b23fcf4a654
SanhoLee <stlsh0516@gmail.com>
2019-07-01 23:19:25 +0900

    3.9 Editing a Video

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -40,25 +40,51 @@ export const postUpload = async (req, res) => {
     title,
     description
   });
-  res.redirect(routes.videoDetail(newVideo.id))
+  res.redirect(routes.videoDetail(newVideo.id));
 };
 
-export const videoDetail = async (req, res) =>{
+export const videoDetail = async (req, res) => {
   const {
-    params:{id}
+    params: { id }
   } = req;
   try {
     const video = await Video.findById(id);
-    res.render("videoDetail", { pageTitle: "Video Detail" , video});
+    res.render("videoDetail", { pageTitle: "Video Detail", video });
   } catch (error) {
     res.redirect(routes.home);
   }
   // 비디오 아이디에 존재하지 않는 정보가 들어오면 홈으로 리다이렉팅
-}
-  
+};
 
-export const editVideo = (req, res) =>
-  res.render("editVideo", { pageTitle: "Edit Video" });
+export const getEditVideo = async (req, res) => {
+  const {
+    params: { id }
+  } = req;
+  try {
+    // id로 먼저 편집 할 비디오를 찾고,
+    const video = await Video.findById(id);
+    // 편집 비디오 페이지 렌더링!
+    res.render("editVideo", { pageTitle: `Edit ${video.title}`, video });
+  } catch (error) {
+    res.redirect(routes.home);
+  }
+};
+
+export const postEditVideo = async (req, res) => {
+  const {
+    params: { id },
+    body: { title, description }
+  } = req;
+  try {
+    // title:title, description:description 의 표현이 아래와 같이 되었음.
+    // id는 그대로 이고, title, description을 수정하면 다시 redirect 되는 구조?
+    // findOneAndUpdate를 쓰면 변경하고자 하는 내용을? 먼저 저장을 해야된다고 함.
+    await Video.findOneAndUpdate({ id }, { title, description });
+    res.redirect(routes.videoDetail(id));
+  } catch (error) {
+    res.redirect(routes.home);
+  }
+};
 
 export const deleteVideo = (req, res) =>
   res.render("deleteVideo", { pageTitle: "Delete Video" });
