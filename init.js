29c43d1adf6ce69fd29ee6eaae9c43a0114ff9e6
SanhoLee <stlsh0516@gmail.com>
2020-04-23 20:40:13 +0900

    8.0 Starting the Video Player

--- init.js
+++ init.js
@@ -1,15 +1,12 @@
 import "./db";
 import dotenv from "dotenv";
 import app from "./app";
-
-dotenv.config();
-// .env파일에 변수들 저장되있음. 보안관련 변수들을 여기다 저장해놓고, 댕겨와서 사용.
-
 import "./models/Video";
 import "./models/Comment";
 import "./models/User";
 
-// importing app object from app.js file
+dotenv.config();
+// .env파일에 변수들 저장되있음. 보안관련 변수들을 여기다 저장해놓고, 댕겨와서 사용.
 
 const PORT = process.env.PORT || 4000;
 
