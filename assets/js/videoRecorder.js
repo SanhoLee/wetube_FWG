1d8e274b88b251c2181a5c5827f3812ba2a45a8f
SanhoLee <stlsh0516@gmail.com>
2020-04-27 08:13:33 +0900

    9.1 Recording Video part One

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -2,7 +2,22 @@ const recorderContainer = document.getElementById("jsRecordContainer");
 const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById("jsVideoPreview");
 
-const startRecording = async () => {
+// streamObject는 전역변수로 선언
+let streamObject;
+
+const handleVideoData = event => {
+  console.log(event);
+};
+
+const startRecording = () => {
+  const videoRecorder = new MediaRecorder(streamObject);
+  videoRecorder.start();
+  videoRecorder.addEventListener("dataavailable", handleVideoData);
+  // videoRecorder.ondataavailable = handleVideoData;
+  console.log(videoRecorder);
+};
+
+const getVideo = async () => {
   try {
     const stream = await navigator.mediaDevices.getUserMedia({
       audio: true,
@@ -11,14 +26,20 @@ const startRecording = async () => {
     videoPreview.srcObject = stream;
     videoPreview.muted = true;
     videoPreview.play();
+    // stream contents가 화면으로만 출력되고 있지만, 실제로 데이터로 전송하기 위해 아래 함수를 이용
+    streamObject = stream;
+    startRecording();
+    recordBtn.innerHTML = "Stop Recording";
   } catch (error) {
     recordBtn.innerHTML = "😫 Cant Record";
-    recordBtn.removeEventListener("click", startRecording);
+    recordBtn.removeEventListener("click", getVideo);
+  } finally {
+    recordBtn.removeEventListener("click", getVideo);
   }
 };
 
 function init() {
-  recordBtn.addEventListener("click", startRecording);
+  recordBtn.addEventListener("click", getVideo);
 }
 
 if (recorderContainer) {
