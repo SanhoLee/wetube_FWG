240bbd6d456547e98aaec82a6bec331aca73dcf4
SanhoLee <stlsh0516@gmail.com>
2020-04-27 22:37:42 +0900

    9.2 Recording video part Two

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -2,19 +2,36 @@ const recorderContainer = document.getElementById("jsRecordContainer");
 const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById("jsVideoPreview");
 
-// streamObject는 전역변수로 선언
+// Global varialbes...
 let streamObject;
+let videoRecorder;
 
 const handleVideoData = event => {
-  console.log(event);
+  // download videa with link data.
+  // Blob data -> It is made by 0 and 1.
+  const { data: videoFile } = event;
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(videoFile);
+  link.download = "recorded.webm";
+  document.body.appendChild(link);
+  link.click();
+};
+
+const stopRecording = () => {
+  videoRecorder.stop();
+  recordBtn.removeEventListener("click", stopRecording);
+  recordBtn.addEventListener("click", getVideo);
+  recordBtn.innerHTML = "Start Recording";
 };
 
 const startRecording = () => {
-  const videoRecorder = new MediaRecorder(streamObject);
+  videoRecorder = new MediaRecorder(streamObject);
   videoRecorder.start();
   videoRecorder.addEventListener("dataavailable", handleVideoData);
   // videoRecorder.ondataavailable = handleVideoData;
-  console.log(videoRecorder);
+  // Once video is stopped, it fired event for dataavailabe.
+  // setTimeout(() => videoRecorder.stop(), 5000);
+  recordBtn.addEventListener("click", stopRecording);
 };
 
 const getVideo = async () => {
