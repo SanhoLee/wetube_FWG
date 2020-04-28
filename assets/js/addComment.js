c67409d0953f03b7b2d29ad0566a35a35d862d00
SanhoLee <stlsh0516@gmail.com>
2020-04-28 21:02:38 +0900

    10.4 API Adding a Comment part Three

--- assets/js/addComment.js
+++ assets/js/addComment.js
@@ -1,5 +1,26 @@
 import axios from "axios";
+
 const addCommentForm = document.getElementById("jsAddComment");
+const commentList = document.getElementById("jsCommentList");
+const commentNumber = document.getElementById("jsCommentNumber");
+
+const increaseNumber = () => {
+  commentNumber.innerHTML = parseInt(commentNumber.innerHTML, 10) + 1;
+  // commentNumber.innerHTML의 type이 String이기 때문에 int 형태로 파싱해줌.
+};
+
+const addComment = comment => {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerHTML = comment;
+  li.appendChild(span);
+  commentList.prepend(li);
+  // 이렇게만 두면, session refresh했을 때, 최근에 작성한 comment가 가장 밑으로 가게 된다.
+  // 그래서, videodetail에서 video.comment.reverse() 메소드를 사용해서 개선, array의 메소드 reverse()를 사용해서 뒤에서부터 loop element를 뿌려주도록 함.
+
+  // increasing number for real-time
+  increaseNumber();
+};
 
 const sendComment = async comment => {
   const videoId = window.location.href.split("/videos/")[1];
@@ -13,6 +34,10 @@ const sendComment = async comment => {
   //   data:{comment} 는 comment:comment 란 뜻인데, 앞쪽 comment는,
   // videoController의 postAddComment에서 req.body.comment 의 comment를 참조해서 가져온다.
   // 즉, req.body.comment = comment(here, the comment we texted on) 와 같게 된다.
+  if (reponse.status === 200) {
+    // axios로 부분적인 status만 가지고 와서, 아래 함수를 실행시킴, 전체적인 페이지의 리소소를 다시 불러들이진 않음.
+    addComment(comment);
+  }
 };
 
 const handleSubmit = event => {
